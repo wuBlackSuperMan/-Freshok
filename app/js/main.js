e83824f4eb44a8cd0fa162d2d7890f4552ceb67b
wuBlackSuperMan <andriyuk999@gmail.com>
2021-09-24 15:15:56 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,7 @@
+$(function () {
+  $('.slider__list').slick({
+    prevArrow: '<button type="button" class="slick__btn slick-prev"><img src="images/slider/prevarrow.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick__btn slick-next"><img src="images/slider/nextarrow.svg" alt=""></button>',
+  });
+
+})
